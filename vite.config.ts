c33c96a6943abfda26f74b4ba3b6e93ad3120f5e
aishenko <qialex.i@gmail.com>
2026-02-21 22:28:28 +0100

    build updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/react-gallery/' : '/',
   plugins: [react()],
-  build: {
-    outDir: 'build'
-  },
   server: {
     port: 3000
   }
